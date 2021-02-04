fc7aff5933d53bfb4c6e5bbf503486c45e50d672
asgerkrabbe <asgerkrabbe@gmail.com>
2021-02-04 10:29:27 +0100

    First commit

---
+++ src/com/kea/CatTest.java
@@ -0,0 +1,13 @@
+package com.kea;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatTest {
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+
+
+
+    }
+}
---
+++ src/com/kea/Main.java
@@ -0,0 +1,11 @@
+package com.kea;
+
+public class Main {
+
+    public static void main(String[] args) {
+Cat cat = new Cat("Test","test",123,123,"hey",true);
+Dog dog = new Dog("Test","test",123,123,"hey","Asger");
+        System.out.println(cat);
+        System.out.println(dog);
+    }
+}
